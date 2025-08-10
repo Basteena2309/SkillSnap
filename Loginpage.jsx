c53f244e3c9be0d58b8ac2e42a28e6af48917458
Basteena2309 <23basteena@stalphonsa.edu.in>
2025-08-10 11:31:32 +0530

    Create Loginpage.jsx

---
+++ Loginpage.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:3000/api/login', {
+        email,
+        password,
+      });
+
+      localStorage.setItem('token', res.data.token);
+      navigate('/lessons');
+    } catch (err) {
+      alert('Login failed');
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginPage;
